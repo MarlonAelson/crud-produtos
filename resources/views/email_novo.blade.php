90214399e8c01baf2d675477af5fcc29cd2172ca
marlon note <marlon.aelson.gomes@gmail.com>
2021-12-05 21:54:25 -0300

    blade teste de email

---
+++ resources/views/email_novo.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<label for="">Para:</label>
+<input type="text">
+<label for="">Cc:</label>
+<input type="text">
+<label for="">Cco:</label>
+<input type="text">
+<label for="">Assunto:</label>
+<input type="text">
+<label for="">Anexos:</label>
+<input type="file">
+<label for="">Mensagem</label>
+<textarea name="" id="" cols="30" rows="10">
+
+</textarea>
+</body>
+</html>
